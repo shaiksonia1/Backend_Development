28b1d9b6a2bc4209e4d68439bf6b0c8a8ceca5ea
Shaik Sonia <shaiksonia222@gmail.com>
2024-10-06 02:34:42 +0530

    nodeJS_learning

---
+++ NodeJS_Learning/3.2 express/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+ const app  = express();
+ const port = 3000;
+
+ app.get("/", (req,res)=>{
+   console.log(req.rawHeaders);
+ });
+
+ app.listen(port, ()=>{
+    console.log(`server is running on ${port}`)
+ });
