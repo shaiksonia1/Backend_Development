6bbef02326e11fd5ebaf3dca2388b6551da66a52
Shaik Sonia <shaiksonia222@gmail.com>
2024-10-06 02:47:58 +0530

    nodeJS_Learning

--- NodeJS_Learning/3.2 express/index.js
+++ NodeJS_Learning/3.2 express/index.js
@@ -3,7 +3,17 @@ import express from "express";
  const port = 3000;
 
  app.get("/", (req,res)=>{
-   console.log(req.rawHeaders);
+res.send("<h1>hello world!</h1>");
+ });
+ app.get("/homePage", (req,res)=>{
+res.send("<h1>homePage!</h1>");
+ });
+ app.get("/contact", (req,res)=>{
+res.send("<h1>Contact</h1>");
+ });
+
+ app.get("/about", (req,res)=>{
+res.send("<h1>About me!</h1>");
  });
 
  app.listen(port, ()=>{
